ee789bf8be1de68aaff0a1f150dd9825a8d01c8a
Dell <erick.hs@icloud.com>
2024-04-04 20:29:10 -0600

    sql code

--- main.py
+++ main.py
@@ -5,57 +5,15 @@ conn = mysql.connector.connect(
     user='root',
     host='localhost',
     password='Dog=cat6',
-    database='bankClients',
+    database='c2cproject',
     
 )
 
 mycursor = conn.cursor()
 # Create a new record
-sql = """INSERT INTO client (email, password) VALUES ('john@example.com', 'mypassword')"""
-try:
-    mycursor.execute(sql)
-    # Commit changes
-    conn.commit()
+sql = """INSERT INTO client (email, password) VALUES ('jtesting, 'connection')"""
 
-except:
-    conn.rollback()
-conn.close()
-print("working")
-from tkinter import *
-from tkinter import ttk
+mycursor.execute(sql)
+# Commit changes
+conn.commit()
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
-
-root = Tk()
-root.title("Feet to Meters")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind("<Return>", calculate)
-
-root.mainloop()
